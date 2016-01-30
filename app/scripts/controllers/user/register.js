d931e91d7b5f2e852c9c470dea0014d7ed2c3969
Jérémy De Sousa <jeje3842@gmail.com>
2016-01-30 01:37:36 +0100

    Ajout de style + ajout de clients

--- app/scripts/controllers/user/register.js
+++ app/scripts/controllers/user/register.js
@@ -8,10 +8,56 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('UserRegisterCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('UserRegisterCtrl', function ($scope, $http) {
+    $scope.open2 = function () {
+      $scope.popup2.opened = true;
+    };
+
+    $scope.reset = function () {
+      $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.register = function () {
+      var client = {
+        'nom': $scope.user.nom,
+        'prenom': $scope.user.prenom,
+        'date_naissance': $scope.user.date_naissance
+      };
+
+      $http({
+        url: 'http://localhost:3000/api/client',
+        method: "PUT",
+        dataType: 'json',
+        contentType: "application/json",
+        data: client,
+      })
+        .then(function successCallback(response) {
+          if (response.data.status) {
+            $scope.reset();
+            $scope.addAlert("Le client a bien été enregistré !", "success");
+          } else {
+            $scope.addAlert("Une erreur est survenue", "error");
+          }
+        }, function errorCallback(response) {
+          $scope.addAlert("Une erreur est survenue", "error");
+        });
+    };
+
+    $scope.alerts = [];
+
+    $scope.addAlert = function (msg, type) {
+      $scope.alerts.push({msg: msg, type: type});
+    };
+
+    $scope.closeAlert = function (index) {
+      $scope.alerts.splice(index, 1);
+    };
+
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+    $scope.popup2 = {
+      opened: false
+    };
   });
